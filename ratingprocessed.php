3466c487d2da72774503f5ffc993782b2342da0b
Matthew Wolf <mwolf@carrollcc.edu>
2020-04-25 12:05:19 -0400

    include all changes

--- ratingprocessed.php
+++ ratingprocessed.php
@@ -1,33 +1,46 @@
 <!DOCTYPE html>
-<html>
-<?php require("head.php");?>
-<body>
-
-        <!--Bootstrap navbar-->
-<?php require("nav.php");?>
-
-    <div class="container-fluid">
 <?php
-        require("base.php");
-        
-    extract($_GET);
-   
-    if(!empty($movie_name)){
-
-    $Movie = new Movie($movie_name,$director_name,$artist,$genre,$rating);
-        if (!$FileMovieManager->create($Movie)) {
-            $message = <<<EOT
-            <div class="alert alert-danger" role="alert">Failed to write to file.</div>
-            EOT;
-            } 
-        else {
-                $message = <<<EOT
-                <div class="alert alert-success" role="alert">Success!</div>
-                EOT;
-        }
-    }
-    echo $message;
+    $mname=$_POST['mname'];
+    $dname=$_POST['dname'];
+    $artist=$_POST['artist'];
+    $genre=$_POST['genre'];
+    $rating=$_POST['rating'];
+    $document_root = $_SERVER['DOCUMENT_ROOT'];
+    $date = date('H:i, jS F Y');
 ?>
-        </div>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>MovieRater - Rating Confirmation</title>
+</head>
+<body>
+    <h1>MovieRater</h1>
+    <h2>Rating Confirmed!</h2>
+    <?php
+        echo '<p>Rating Confirmed at ';
+        echo date('H:i jS F Y');
+        echo '</p>';
+            
+        echo '<p>Your rating was processed with the following details:</p>';
+        echo 'Movie Name:'. htmlspecialchars($mname).'<br/>';
+        echo 'Director Name:'. htmlspecialchars($dname).'<br/>';
+        echo 'Artists:'. htmlspecialchars($artist). '<br/>';
+        echo 'Genre:'. htmlspecialchars($genre). '<br/>';
+        echo 'Rating:'. htmlspecialchars($rating). '<br/>';
+
+
+        
+
+        @$fp = fopen("$document_root/ratings.txt", 'ab');
+
+        $outputstring = $date."\t".$mname." :Movie Name \t".$dname." :Director Name \t".$artist." :Artist \t".$genre. " :Genre \t".$rating." :Rating \n";
+
+
+        fwrite($fp, $outputstring, strlen($outputstring));
+        fclose($fp);
+
+        echo "<p>Rating Written</p>";
+
+?>
 </body>
 </html>
--- ratingresults.php
+++ ratingresults.php
@@ -1,55 +1,22 @@
 <!DOCTYPE html>
 <?php 
-
+$document_root=$_SERVER['DOCUMENT_ROOT'];
 ?>
 <html>
-<?php require("head.php");?>
+<head>
+    <meta charset="utf-8" />
+    <title>MovieRater - Rating Results</title>
+</head>
 <body>
-
-        <!--Bootstrap navbar-->
-<?php require("nav.php");?>
-
-    <div class="container">
     <h1>Movie Rater: Rating Results</h1>
     <h2>Movie Ratings:</h2>
 
 <?php
-require("base.php");
-$list = $fileMovieManager->read();
-$ratings = explode("\n", trim($list));
-$table_body = '';
-foreach($ratings as $movierating) {
-        $table_body .= '<tr>';
-        $movie_name = explode(',', trim($movierating));
-        $table_body .= '<td>' .$movie_name[0] . '</td>';
-        $table_body .= '<td>' .$movie_name[1] . '</td>';
-        $table_body .= '<td>' .$movie_name[2] . '</td>';
-        $table_body .= '<td>' .$movie_name[3] . '</td>';
-        $table_body .= '<td>' .$movie_name[4] . '</td>';
-        $table_body .= '<td><a href="edit.php?id=' . $key . '" class=btn btn-info btn-sm">Edit</a></td>';
-        $table_body .= '</tr>';
-}
+    @$fp = fopen("$document_root/ratings.txt", 'rb');
+    $rating= fgets($fp);
+    echo htmlspecialchars($order). "<br />";
+
+    fclose($fp);
 ?>
- 
-        <div class="row">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Movie Name</th>
-                        <th>Director Name</th>
-                        <th>Artist</th>
-                        <th>Genre</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php echo $table_body; ?>
-
-                </tbody>
-            </table>
-
-        </div>
-    
-    </div>
 </body>
 </html>
